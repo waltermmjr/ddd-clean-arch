6275cacc9eb493997c88656e9a778d4bfdbd0c3d
walter <walter.mousinho@gmail.com>
2026-04-09 20:06:22 -0300

    criação do arquivo PacientList.tsx e do diretorio pacientes/page.tsx

---
+++ src/app/pacientes/page.tsx
@@ -0,0 +1,16 @@
+import { PacientList } from "@/modules/Paciente/Components/PacientList";
+
+export default function PacientesPage(){
+
+    return (
+
+        <div>
+
+            <h1> PAGINA DE PACIENTES</h1>
+            <hr />
+            <PacientList />
+
+
+        </div>
+    )
+}
