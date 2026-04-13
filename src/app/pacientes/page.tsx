2134f6df076938ef5ca0fa9fb05d4d1f4ff3e4c2
walter <walter.mousinho@gmail.com>
2026-04-13 20:20:53 -0300

    Pagina PacienteForm funcionando

---
+++ src/app/pacientes/page.tsx
@@ -0,0 +1,17 @@
+import { PacienteForm } from "@/modules/Paciente/Components/PacienteForm"
+import "@src/app/estilo.css"
+
+
+export default function PagePacientes(){
+    
+    return(
+        
+        <div>
+
+            <PacienteForm />
+
+        </div>
+
+    )
+    
+}
